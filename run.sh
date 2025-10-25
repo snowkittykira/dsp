1734f2f35eb7b68dfb75fb7bbd6911193ed2115c
Kira Boom <snowkittykira@gmail.com>
2025-10-25 10:31:11 -0400

    windows and osx build workflows

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 set -e
 
-cmake -B build -DCMAKE_BUILD_TYPE=Release
-cmake --build build
+cmake -B build
+cmake --build build --config Release
 npx tstl
 cp build/dsp.so build/game/dsp.so
 (cd build/game/ && lovr .)
